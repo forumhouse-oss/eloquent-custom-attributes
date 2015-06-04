36691dd3c6e898a75bb81af0237dba6dca68f3a0
Vladislav Rastrusny <FractalizeR@yandex.ru>
2015-06-04 11:20:36 +0300

    PostgresTypes\PostgresArrayAttrHandlerTrait added to handle Postgres array attributes

---
+++ src/PostgresTypes/PostgresArrayAttrHandlerTrait.php
@@ -0,0 +1,88 @@
+<?php namespace FHTeam\EloquentCustomAttrs\PostgresTypes;
+
+use Exception;
+use FHTeam\EloquentCustomAttrs\ArrayAttributeWrapper;
+
+/**
+ * Class PostgresArrayAttrHandlerTrait
+ *
+ * @package PostgresTypes
+ */
+class PostgresArrayAttrHandlerTrait
+{
+    /**
+     * @var array
+     */
+    protected $postgresArrayAttrWrappers = [];
+
+    /**
+     * @param string $key
+     *
+     * @return array
+     */
+    public function handleGetAttributePostgresArray($key)
+    {
+        if (isset($this->postgresArrayAttrWrappers[$key])) {
+            return $this->postgresArrayAttrWrappers[$key];
+        }
+
+        $wrapper = new ArrayAttributeWrapper(
+            $this,
+            $key,
+            $this->postgresArrayToPhpArray(parent::getAttribute($key))
+        );
+
+        $this->postgresArrayAttrWrappers[$key] = $wrapper;
+
+        return $wrapper;
+    }
+
+    /**
+     * @param string $key
+     * @param array  $value
+     *
+     * @throws \Exception
+     */
+    public function handleSetAttributePostgresArray($key, $value)
+    {
+        parent::setAttribute($key, $this->phpArrayToPostgresArray($value));
+    }
+
+    /**
+     * Unpacks geometric POINT type data representation for ex. '(1.2,3.4)'
+     *
+     * @param string $data Data to unpack as a point
+     *
+     * @return array [X, Y]
+     */
+    public function postgresArrayToPhpArray($data)
+    {
+        $data = trim($data, "{} \t\n\r\0\x0B");
+        if ('' === $data) {
+            return null;
+        }
+
+        return explode(',', $data);
+    }
+
+    /**
+     * Packs geometric POINT type data representation for ex. '(1.2,3.4)'
+     *
+     * @param array $data Array of coordinates for the point - [X, Y]
+     *
+     * @return string
+     * @throws Exception
+     */
+    public function phpArrayToPostgresArray($data)
+    {
+        if (null == $data) {
+            return null;
+        }
+
+        if (count($data) !== 2) {
+            throw new Exception("Point should have only two coordinates");
+        }
+
+        return '{'.implode(',', $data).'}';
+    }
+}
